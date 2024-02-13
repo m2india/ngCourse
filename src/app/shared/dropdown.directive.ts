760b2d8de5022855a8e48f1af3ee68a290239d9d
Manimaran <manimaraan.r@gmail.com>
2024-02-13 11:55:34 +0530

    closing the dropdown anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,20 +1,24 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
- @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.open') isOpen = false;
 
-  constructor() { }
+  // @HostListener('click') toggleOpen(){
+  //   this.isOpen = !this.isOpen;
+  // }
 
-  @HostListener('click') toggleOpen(){
-    this.isOpen = !this.isOpen;
+  // @HostListener('mouseleave') mouseleave(){
+  //   this.isOpen = false;
+  // }
+
+  @HostListener('document:click',['$event']) toggleOpen(event: Event){
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen: false;
   }
 
-  @HostListener('mouseleave') mouseleave(){
-    this.isOpen = false;
-  }
+  constructor(private elRef: ElementRef) { }
 
 }
